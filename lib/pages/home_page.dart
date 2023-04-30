d8741c35d07777afb04d05526fd85fd25ec05ea5
AhnAne007 <ahnaf.anis@gmail.com>
2023-04-30 18:38:34 +0500

    creating HomePage

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: const Column(
+        // app bar
+
+        // cards
+
+        //row of options
+
+        //column of options
+
+        // navigation bar
+      ),
+    );
+  }
+}
